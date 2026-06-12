84d5fa00d41b5760e5d661ad1342bf69e10ed7a8
Nolan Augustine <nolanaug@gmail.com>
2026-06-12 12:20:58 +0200

    Remove duplicate src lighthouse package

--- src/lighthouse/main.py
+++
@@ -1,8 +0,0 @@
-﻿from lighthouse.collectors.system import collect_system_snapshot
-
-def main():
-    snapshot = collect_system_snapshot()
-    print(snapshot)
-
-if __name__ == "__main__":
-    main()
